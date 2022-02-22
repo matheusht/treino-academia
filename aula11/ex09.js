0b10b473c7cad3d39e2fad5c6f1a724893d29b31
matheusht <96798246+matheusht@users.noreply.github.com>
2022-02-22 15:47:02 -0300

    Delete aula11 directory

--- aula11/ex09.js
+++
@@ -1,7 +0,0 @@
- var rel = 'Evangelico'
- console.log(`sou ${rel}`)
- if (rel == 'Cristão') {
-     console.log('você é filho de jesus cristo')
- } else {
-     console.log('Jesus cristo morreu na cruz por um IMBECIL')
- }
